da164e84eeceecc7538fdb74d3d9173993512ccd
ZePotente <ezeposada@gmail.com>
2020-05-15 19:54:27 -0300

    Server de notificacion de recepcion hardcodeado.

--- Emisor/Client/src/Modelo/InternetManager.java
+++ Emisor/Client/src/Modelo/InternetManager.java
@@ -1,7 +1,5 @@
 package Modelo;
 
-import Modelo.Mensaje.Mensaje;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,9 +13,36 @@ import java.net.UnknownHostException;
 import java.util.Observable;
 
 public class InternetManager extends Observable {
-    private ServerRecepcion recepcion;
+    private static final int PUERTO_NOTIFICACION_RECEPCION = 300;
+
     public InternetManager() {
-        recepcion = new ServerRecepcion().start();
+        escuchar(InternetManager.PUERTO_NOTIFICACION_RECEPCION);
+    }
+    
+    public void escuchar(int puerto) {
+        new Thread() {
+            public void run() {
+                try {
+                ServerSocket sv = new ServerSocket(puerto);
+                while (true) {
+                    try(Socket socket = sv.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) 
+                    {
+                            String nombreDestinatario = in.readLine();
+                            setChanged();
+                            notifyObservers(nombreDestinatario);
+                    } catch (IOException e) {
+                        //tampoco deberia ir aca.
+                        System.out.println("Error al recibir una notificacion de recepcion.");
+                    }
+    
+                }
+                } catch (Exception e) {
+                    // lastima
+                    System.out.println("Algo fallo en el server de notificacion de mensajes con recepcion.");
+                }
+            }
+        }.start();
     }
     
     public void enviarMensaje(String nombreDestinatario, String nroIP, int nroPuerto, String msg) throws UnknownHostException, IOException {
--- Emisor/Client/src/Modelo/ServerRecepcion.java
+++ Emisor/Client/src/Modelo/ServerRecepcion.java
@@ -1,4 +1,30 @@
 package Modelo;
 
-public class ServerRecepcion {
+import java.io.IOException;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerRecepcion extends Thread {
+    private ServerSocket sv;
+    private static int port;
+    
+    public ServerRecepcion() throws IOException {
+        sv = new ServerSocket(ServerRecepcion.port);
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            try(Socket socket = sv.accept();) {
+                setChanged();
+                notifyObservers(nombreDestinatario);
+            } catch (IOException e) {
+                //tampoco deberia ir aca.
+                System.out.println("Error al recibir una notificacion de recepcion.");
+            }
+
+        }
+    }
 }
